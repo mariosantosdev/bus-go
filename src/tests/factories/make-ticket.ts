a4a7962d7ddd7dc9fd82c028f0cb3a78129b0563
Mário Santos <mariodev7@gmail.com>
2024-01-12 20:36:36 -0300

    test: create factory ticket entity

--- src/tests/factories/make-ticket.ts
+++ src/tests/factories/make-ticket.ts
@@ -3,10 +3,12 @@ import {
   TicketProps,
 } from 'src/domain/application/entities/ticket.entity';
 import { makeTraveler } from './make-traveler';
+import { faker } from '@faker-js/faker';
 
 export function makeTicket(overrides: Partial<TicketProps> = {}) {
   return Ticket.create({
     traveler: makeTraveler(),
+    value: Number(faker.commerce.price()),
     ...overrides,
   });
 }
