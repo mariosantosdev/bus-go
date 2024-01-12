995ad0f75ab79b343513101d049027d46c73e85f
Mário Santos <mariodev7@gmail.com>
2024-01-12 17:35:40 -0300

    feat: create driver entity

---
+++ src/domain/application/entities/driver.entity.ts
@@ -0,0 +1,23 @@
+import { Entity } from '~/core/entities/entity';
+import { ID } from '~/core/entities/id';
+
+interface DriverProps {
+  name: string;
+  driverLicense: string;
+}
+
+export class Driver extends Entity<DriverProps> {
+  get name() {
+    return this.props.name;
+  }
+
+  get driverLicense() {
+    return this.props.driverLicense;
+  }
+
+  static create(props: DriverProps, id?: ID) {
+    const driver = new Driver(props, id);
+
+    return driver;
+  }
+}
